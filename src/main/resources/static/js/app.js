86cc96463ba11de7e28a9483aa046118fe1ee3f2
unknown <mateomejia401@gmail.com>
2022-03-14 22:26:18 -0500

    Primer punto

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,7 +1,8 @@
 var Module = (function () {
     let author;
-    let blueprintname;
     var consulta=apimock;
+    var lastxlist=null;
+    var lastylist=null;
     function getName(){
         author = document.getElementById("Autor").value;
         $("#autorLabel").text(author);
@@ -38,17 +39,46 @@ var Module = (function () {
         ctx.clearRect(0, 0, c.width, c.height);
         ctx.restore();
         ctx.beginPath();
-        for (let i = 1; i < puntos.length; i++) {
+        /*for (let i = 1; i < puntos.length; i++) {
             ctx.moveTo(puntos[i - 1].x, puntos[i - 1].y);
             ctx.lineTo(puntos[i].x, puntos[i].y);
             if (i === puntos.length - 1) {
                 ctx.moveTo(puntos[i].x, puntos[i].y);
                 ctx.lineTo(puntos[0].x, puntos[0].y);
             }
+        }*/
+        for(let i =0; i<puntos.length-1; i++){
+            ctx.moveTo(puntos[i].x, puntos[i].y);
+            ctx.lineTo(puntos[i+1].x, puntos[i+1].y);
+            lastxlist = puntos[i+1].x;
+            lastylist = puntos[i+1].y;
         }
         ctx.stroke();
         $("#currentName").text(planoSeleccionado);
     }
+    var draw = function (){
+        const canvas = document.getElementById("MyCanvas");
+        const contexto = canvas.getContext("2d");
+        const color = black;
+        const grosor = 2;
+        let xActual = 0, yActual = 0;
+        const obtenerXReal = clientX - canvas.getBoundingClientRect().left;
+        const obtenerYReal = clientY - canvas.getBoundingClientRect().top;
+        canvas.addEventListener("mousedown",evento=function (){
+                xActual = obtenerXReal;
+                yActual = obtenerYReal;
+                contexto.moveTo(lastxlist, lastylist);
+                contexto.lineTo(xActual, yActual);
+                contexto.fillStyle=color;
+                contexto.stroke();
+                lastxlist = xActual;
+                lastylist = yActual;
+
+        },false);
+
+        };
+
+
     return {
         planosAutor: function (){
             consulta.getBlueprintsByAuthor(getName(),_blueprintData);
@@ -56,7 +86,8 @@ var Module = (function () {
         drawPlan: function(name) {
             obra = name;
             consulta.getBlueprintsByNameAndAuthor(getName(),obra,pintaparcero);
-        }
+        },
+        funListener: draw
     };
 })();
 
